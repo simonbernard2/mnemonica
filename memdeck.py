fc6582a1aa6be715f5e5ceeb7bb7148c5c49af1c
Simon Bernard <simonbernard@gmail.com>
2020-12-20 15:22:40 -0500

    ajout color

--- memdeck.py
+++ memdeck.py
@@ -1,15 +1,11 @@
-#J'essaie de faire un programme qui simule un jeu de cartes pour faire des recherches sur le memdeck
-#test de git
 import random
 import itertools
 from typing import List
+from termcolor import colored, cprint
 import inspect
 from collections import Counter
 
 
-#lire sur map et filter
-
-#remplacer str par enums(value: string/int)(suit: color/suit)
 # __eq__ to compare cards with one another
 class Card:
     def __init__(self, suit: str, value: str) -> None:
@@ -17,13 +13,24 @@ class Card:
         self.value = value
 
     def __repr__(self) -> str:
-      return f"{self.value} of {self.suit}"
+        if self.suit == 'spades':
+            suit = '♠'
+            return colored(f"{self.value}{suit}", 'blue')
+        if self.suit == 'clubs':
+            suit = '♣'
+            return colored(f"{self.value}{suit}", 'green')
+        if self.suit == 'hearts':
+            suit = '♥'
+            return colored(f"{self.value}{suit}", 'red')
+        if self.suit == 'diamonds':
+            suit = '♦'
+            return colored(f"{self.value}{suit}", 'magenta')
 
-    # def __eq__(self, other): 
-    #     if not isinstance(other, Card):
-    #     # don't attempt to compare against unrelated types
-    #      return NotImplemented
-
+    def __eq__(self, other):
+        if not isinstance(other, Card):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return self.suit == other.suit and self.value == other.value
 
 
 class Deck:
@@ -36,9 +43,9 @@ class Deck:
     def deal(self) -> Card:
         return self.cards.pop(0)
 
-    def bottomDeal(self) -> Card:
+    def bottom_deal(self) -> Card:
         return self.cards.pop()
-    
+
     def faro(self) -> 'Deck':
         if len(self.cards) % 2 != 0:
             return False
@@ -49,36 +56,36 @@ class Deck:
             self.cards = list(itertools.chain(*zip(topHalf, bottomHalf)))
         return self
 
-    def cutCards(self,number) -> "packet":
-        packet=[]
+    def cut_cards(self, number) -> "packet":
+        packet = []
         for n in range(number):
             card = self.deal()
             packet.append(card)
         return packet
-    
-    def dealIntoPiles(self,cards: int, piles: int):
+
+    def deal_into_piles(self, cards: int, piles: int):
         piles = [[] for n in range(piles)]
 
         for _ in range(cards):
             for pile in piles:
                 pile.append(self.cards.pop(0))
         return piles
-    
-    def findMates(self,value:str):
+
+    def find_mates(self, value: str):
         positions = []
         for card in self.cards:
             if value == card.value:
                 positions.append(card)
         return positions
 
-    def findSuit(self, suit:str):
+    def find_suit(self, suit: str):
         positions = []
         for card in self.cards:
             if suit == card.suit:
                 positions.append(card)
         return positions
 
-    def findColor(self, color:str):
+    def find_color(self, color: str):
         positions = []
         if color == "red":
             for card in self.cards:
@@ -88,124 +95,86 @@ class Deck:
                     positions.append(card)
         if color == "black":
             for card in self.cards:
-                if card.suit == "hearts":
+                if card.suit == "spades":
                     positions.append(card)
-                if card.suit == "diamonds":
+                if card.suit == "clubs":
                     positions.append(card)
         return positions
 
-    #Doit ben avoir moyen de simplifier ça? m'semble ça fait du self.cards en esti dans même phrase
-    #refere a findMazine
-    def findPairs(self):
+    # Doit ben avoir moyen de simplifier ça? m'semble ça fait du self.cards en esti dans même phrase
+    # refere a findMazine
+    def find_pairs(self):
         pairs = []
-        for card in self.cards[:-1] :
-            if card.value == self.cards[self.cards.index(card)+1].value:
-                pairs.append({f"{card.value} of {card.suit} and {self.cards[self.cards.index(card)+1].suit}": self.cards.index(card)+1})
+        for card in self.cards[:-1]:
+            if card.value == self.cards[self.cards.index(card) + 1].value:
+                pairs.append({
+                    f"{card.value} of {card.suit} and {self.cards[self.cards.index(card) + 1].suit}": self.cards.index(
+                        card) + 1})
         return pairs
 
     def findMazine(self):
-        pairs = []
-        for i in range(0,len(self.cards)-1):
-            if self.cards[i].value == self.cards[i+1].value:
+        for i in range(0, len(self.cards) - 1):
+            if self.cards[i].value == self.cards[i + 1].value:
                 return 0
-    
-    #Doit ben avoir moyen de simplifier ça? m'semble ça fait du self.cards en esti dans même phrase
-    def findThreeOfAKind(self):
+
+    # Doit ben avoir moyen de simplifier ça? m'semble ça fait du self.cards en esti dans même phrase
+    def find_threes_of_a_kind(self):
         threes = []
-        for card in self.cards[:-2] :
-            if card.value == self.cards[self.cards.index(card)+1].value and card.value == self.cards[self.cards.index(card)+2].value:
-                threes.append({f"{card.value} of {card.suit}, {self.cards[self.cards.index(card)+1].suit} and {self.cards[self.cards.index(card)+2].suit}": self.cards.index(card)+1})
+        for card in self.cards[:-2]:
+            condition1 = card.value == self.cards[self.cards.index(card) + 1].value
+            condition2 = card.value == self.cards[self.cards.index(card) + 2].value
+            if condition1 and condition2:
+                card_index = self.cards.index(card)
+                threes.append([card, self.cards[card_index + 1], self.cards[card_index + 2]])
         return threes
 
     def __repr__(self) -> str:
-      deck = ""
-      position=1
-      for card in self.cards:
-        deck += f"{position}: " + repr(card) + "\n"
-        position += 1
-
-      return deck
+        deck = ""
+        position = 1
+        for card in self.cards:
+            deck += f"{repr(card)} \n"
+            # deck += f"{position}: {repr(card)} \n"
+            position += 1
+        return deck
 
     def __len__(self) -> str:
-      return len(self.cards)
-    
+        return len(self.cards)
+
 
 class Player:
     def __init__(self):
         self.hand = []
 
-    def draw(self,deck,number):
+    def draw(self, deck, number):
         for i in range(number):
             card = deck.deal()
             self.hand.append(card)
-        return 
+        return
 
 
-def buildDeckFromFile(filePath: str) -> Deck:
+def build_deck_from_file(filePath: str) -> Deck:
     with open(filePath, 'r') as temp:
-      data = temp.read().splitlines()
-    
+        data = temp.read().splitlines()
+
     cards = []
     for card in data:
-      value, suit = card.split(":")
+        value, suit = card.split(":")
+        cards.append(Card(suit, value))
 
-      cards.append(Card(suit, value))
-    
     return Deck(cards)
+
+
 filePath = "mnemonica.txt"
-deck = buildDeckFromFile(filePath)
+deck = build_deck_from_file(filePath)
 
+card = Card('spades', '5')
+#
+# user_input = int(input(f"Veuillez inscrire un chiffre entre 1 et 52: "))
+# while user_input not in range(1,52):
+#     user_input = int(input(f"Veuillez inscrire un chiffre entre 1 et 52: "))
+# print(deck.cards[user_input-1]
 
-
-######################################################### COMPARING CARDS ################################################
-#comparator 1 (not working)
-if deck.cards[0] == Card("clubs","4"):
-    print("C1: yes")
-else:
-    print("C1: no")
-#comparator 1.1 (not working)
-if deck.cards[0] is Card("clubs","4"):
-    print("C1.1: yes")
-else:
-    print("C1.1: no")
-
-#comparator 2 (not working)
-guess = Card("clubs","4")
-
-if deck.cards[0] == guess:
-    print("C2: yes")
-else:
-    print("C2: no")
-#comparator 2.2 (not working)
-
-if deck.cards[0] is guess:
-    print("C2.2: yes")
-else:
-    print("C2.2: no")
-
-#comparator 3 (works)
-
-if deck.cards[0].suit == guess.suit and deck.cards[0].value == guess.value:
-    print("C3: yes")
-else:
-    print("C3: no")
-#comparator 3.3 
-if deck.cards[0].suit == "clubs" and deck.cards[0].value == "4":
-    print("C3.1: yes")
-else:
-    print("C3.1: no")
-
-
-#I tried both "is" and "==", can't seem to be able to compare "identical" objects without going through their attributes one by one
-
-######################################################### // COMPARING CARDS ################################################
-
-
-
-
-
-deck.shuffle()
-print(deck)
-print(deck.findPairs())
-print(deck.findThreeOfAKind())
-
+print(card)
+print(deck.cards[15])
+print(card == deck.cards[15])
+print(card.suit, deck.cards[15].suit)
